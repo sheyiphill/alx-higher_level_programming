4b87f996cc150089f030e0a73cf779a8dc4af636
Oluwaseyi Oyewole <sheyiphill@gmail.com>
2022-07-06 21:44:21 +0000

    4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
-"""Defines a file-appending function."""
+"""Defines a JSON-to-object function."""
+import json
 
 
-def append_write(filename="", text=""):
-    """Appends a string to the end of a UTF8 text file.
-
-    Args:
-        filename (str): The name of the file to append to.
-        text (str): The string to append to the file.
-    Returns:
-        The number of characters appended.
-    """
-    with open(filename, "a", encoding="utf-8") as f:
-        return f.write(text)
+def from_json_string(my_str):
+    """Return the Python object representation of a JSON string."""
+    return json.loads(my_str)
